e9621c1c06906ccf4d6976be3721336859978f63
Filip Wilczura <filip.wilczura@microsoft.wsei.edu.pl>
2019-11-24 18:10:02 +0100

    Add project files.

---
+++ PubgStats.AppCore/Features/Matches/Queries/GetRecentMatchesForPlayer/GetRecentMatchesForPlayer.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using MediatR.Framework.OperationResults;
+using Pubg.Net;
+
+namespace PubgStats.AppCore.Features.Matches.Queries.GetRecentMatchesForPlayer
+{
+    public class GetRecentMatchesForPlayer : IRequest<RequestResult<PubgMatch>>
+    {
+    }
+}
